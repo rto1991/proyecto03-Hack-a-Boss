c3e2e2f41df348bdfba90812bdbfdc6942c6b8f8
JoffreArias <123706095+joffrea18@users.noreply.github.com>
2023-06-13 23:17:40 +0200

    130623_2317

---
+++ src/MyCloudService.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const MyCloudService = () => {
+    return (
+        <main className='welcome'>
+            <h1>Welcome</h1>
+            {/* <hr /> */}
+            <button className='login'>Login</button>
+            <button className='register'>Register</button>
+        </main>
+    );
+}
+
+export default MyCloudService;
