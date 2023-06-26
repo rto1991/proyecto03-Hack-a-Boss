2f718879bbf5a0af3edfad86838209340a174851
bocanegrapps <hola@informaticabocanegra.com>
2023-06-26 20:45:02 +0200

    intento impl compresión directorios

--- src/controllers/files/downloadFile.js
+++ src/controllers/files/downloadFile.js
@@ -6,6 +6,7 @@ const fs = require("fs");
 const fileUrl = require("url");
 const cors = require("cors");
 const { Stream } = require("stream");
+const compressing = require("compressing");
 
 const downloadFile = async (req, res, next) => {
   let connect;
@@ -39,15 +40,37 @@ const downloadFile = async (req, res, next) => {
 
     const pathToFile = fileUrl.pathToFileURL(path.join(filePath, fileName));
 
+    let finalPath = pathToFile;
 
-    fs.readFile(pathToFile, (err,data) => {
+    if (file[0].is_folder == 1) {
+      //comprimir carpeta y enviar ZIP
+      compressing.tar
+        .compressDir(
+          path.join(filePath, fileName),
+          path.join(filePath, fileName + ".tar")
+        )
+        .then(() => {
+          console.log(
+            fileUrl.pathToFileURL(path.join(filePath, fileName + ".tar"))
+          );
+          finalPath = fileUrl.pathToFileURL(
+            path.join(filePath, fileName + ".tar")
+          );
+        })
+        .catch((err) => {
+          throw err;
+        });
+    }
+    fs.readFile(finalPath, (err, data) => {
       if (err) {
         return next(err);
       }
-      res.setHeader('Content-Disposition','attachment: filename="'+fileName+'"');
+      res.setHeader(
+        "Content-Disposition",
+        'attachment: filename="' + fileName + '"'
+      );
       res.send(data);
-    })
-
+    });
   } catch (error) {
     console.log(error);
     next(error);
